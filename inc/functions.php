5bb5e25915e23510661447b47d77e4515d8f8ada
robmccormack89 <robertmccormack147@gmail.com>
2021-06-24 01:27:46 +0100

    return instead of echo for shortcode output

--- inc/functions.php
+++ inc/functions.php
@@ -1,10 +1,5 @@
 <?php
 
 function cart_result_subtotal() {
-  echo '<span class="header-cart-count">';
-  echo WC()->cart->get_cart_contents_count();
-  echo '</span>';
-  echo '<span class="subtotal-cart">';
-  echo WC()->cart->get_cart_subtotal();
-  echo '</span>';
+  return '<span class="header-cart-count">'.WC()->cart->get_cart_contents_count().'</span><span class="subtotal-cart">'.WC()->cart->get_cart_subtotal().'</span>';
 }
