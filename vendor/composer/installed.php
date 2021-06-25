5fbef109589391b6d78350ba24c9f64ad13001e5
robmccormack89 <robertmccormack147@gmail.com>
2021-06-25 12:25:25 +0100

    vendor added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5bb5e25915e23510661447b47d77e4515d8f8ada',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5bb5e25915e23510661447b47d77e4515d8f8ada',
+    ),
+  ),
+);
