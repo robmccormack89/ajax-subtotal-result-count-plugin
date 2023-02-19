7ac3a3b547b649ffd10c2ec8268cc45211902da8
Robert McCormack <robertmccormack147@gmail.com>
2023-02-19 06:09:30 +0000

    added wrapper

--- inc/functions.php
+++ inc/functions.php
@@ -1,5 +1,5 @@
 <?php
 
 function ajax_cart_result_subtotal() {
-  return '<span class="header-cart-count">'.WC()->cart->get_cart_contents_count().'</span><span class="subtotal-cart">'.WC()->cart->get_cart_subtotal().'</span>';
+  return '<span class="uk-position-relative"><span class="header-cart-count">'.WC()->cart->get_cart_contents_count().'</span><span class="subtotal-cart">'.WC()->cart->get_cart_subtotal().'</span></span>';
 }
